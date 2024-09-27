1d638d62dc2c0eb20dae71362c937f7998ee00cc
ChocZeChoc <nathansl.littlefield@outlook.com>
2024-09-27 18:16:20 +0200

    Done (No test)

---
+++ Assets/Scripts/HandAnimationController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public class HandAnimationController : MonoBehaviour
+{
+
+    public InputActionProperty pinchAnimation;
+    public InputActionProperty fistAnimation;
+
+    public Animator handAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pinchValue = pinchAnimation.action.ReadValue<float>();
+        handAnimation.SetFloat("Pinch", pinchValue);
+
+        float fistValue = fistAnimation.action.ReadValue<float>();
+        handAnimation.SetFloat("Fist", fistValue);
+
+        //Debug.Log("pinch" + pinchValue);
+        //Debug.Log("fist" + fistValue);
+    }
+}
